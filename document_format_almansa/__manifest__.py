e89c8404b8001cb111af3a47f4d4b270d30e6a00
Dani <dani.xtendoo@gmail.com>
2020-11-04 12:28:22 +0100

    document_format_almansa

---
+++ document_format_almansa/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "document_format_almansa",
+    "summary": """Formatos de documentos Almansa Santos""",
+    "version": "12.0.1.0.0",
+    "description": """Formatos de documentos Almansa Santos""",
+    "author": "DDL",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.com",
+    "category": "Extra Tools",
+    "depends": ["base", "account", "sale", "web", "stock", "product",],
+    "license": "AGPL-3",
+    "data": [
+        # layout
+        # "views/layout/external_layout_clean.xml",
+        # delivery
+        "views/delivery/report_delivery_document.xml",
+        # sale_order
+        # "views/sale/report_saleorder_document.xml",
+        # Purchase_order
+        # "views/purchase/report_purchaseorder_document.xml",
+        # Invoice
+        # "views/invoice/report_invoice_document.xml",
+        # Product Label
+        # "views/product_label/paper_format.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
